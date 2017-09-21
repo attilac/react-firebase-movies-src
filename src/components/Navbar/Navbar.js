94f6e1641779bf9e52735cd969c294b74dd49878
attilac <attila.cederbygd@gmail.com>
2017-09-21 12:09:29 +0200

    Some refactoring of UserDropdown

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,24 +2,49 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
+import { Collapse } from 'reactstrap'
 
-function Navbar(props) {
-  const { title, children } = props
-  return <nav className="navbar navbar-toggleable-sm navbar-inverse bg-inverse mb-3">
-    <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>        
-    <Link className="navbar-brand" to="/">{ title }</Link>
-    <div className="collapse navbar-collapse" id="navbarContent">
-      <div className="mr-auto"></div>
-      { children }
-    </div>                        
-  </nav>;        
+class Navbar extends Component {
+  state = {
+    collapsed: true
+  }
+
+  toggleNavbar = () => {
+    this.setState({ collapsed: !this.state.collapsed })
+  }
+
+  render() {
+    const { title, children, isOpen } = this.props,
+      { collapsed } = this.state
+
+    return (
+      <nav className="navbar navbar-toggleable-sm navbar-inverse bg-inverse mb-3">
+        <button 
+          className="navbar-toggler navbar-toggler-right" 
+          type="button" 
+          aria-controls="navbarContent" 
+          aria-expanded="false" 
+          aria-label="Toggle navigation"
+          onClick={ this.toggleNavbar }
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>    
+        <Link className="navbar-brand" to="/">{ title }</Link>
+        <Collapse className="navbar-collapse" isOpen={ !collapsed }>
+          <div className="mr-auto"></div>
+          { children }
+        </Collapse>                       
+      </nav>   
+    ) 
+  }       
 }
 
 Navbar.propTypes = {
+  collapsed: PropTypes.bool,
   title: PropTypes.string,
-  children: PropTypes.array  
+  children: PropTypes.array,
+  isOpen: PropTypes.bool,
+  toggleNavbar: PropTypes.func  
 }
 
 Navbar.defaultProps = {
